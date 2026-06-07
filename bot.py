126b37448240435b1a593d9cf110168765859b6f
realishmugsy <covaciud297@gmail.com>
2026-06-07 21:12:41 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    if message.content.lower() == ".psc":
+        await message.channel.send(
+            """💳 **You want to pay with Paysafe?**
+
+Before sending your codes, please check them here to make sure you copied them correctly:
+https://www.paysafecard.com/de-de/guthabenabfrage/
+
+⚠️ The codes are **not sent to the ticket**. They are only sent via DM to **@kobraempty**, and **only the codes themselves** should be written there.
+
+After sending the codes via DM, please write **"have it"** in the ticket so we know you've sent them."""
+        )
+
+    await bot.process_commands(message)
+
+bot.run("MTUxMzI1NTE5MTkyNTg4MjkxMQ.G-Jq9Q.lpzO9OQ1OzRsFEOpNV2YLipE-Q3lxhdZZFCOAQ")
