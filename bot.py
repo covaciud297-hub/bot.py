d7f83180ddea83b0d823b6161b04c9f599556a51
realishmugsy <covaciud297@gmail.com>
2026-06-09 19:20:16 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,11 +21,28 @@ async def on_message(message):
             """💳 **You want to pay with Paysafe?**
 
 Before sending your codes, please check them here to make sure you copied them correctly:
-https://www.paysafecard.com/de-de/guthabenabfrage/
+https://www.paysafecard.com/en-gb/balance-check/
 
 ⚠️ The codes are **not sent to the ticket**. They are only sent via DM to **@kobraempty**, and **only the codes themselves** should be written there.
 
 After sending the codes via DM, please write **"have it"** in the ticket so we know you've sent them."""
+        )
+
+    elif message.content.lower() == ".pp":
+        await message.channel.send(
+            """💰 **Paypal**
+
+Try To Send On This Account:
+https://www.paypal.me/lgkobramarket
+
+❗ **Before sending:**
+• Make sure to send Friends & Family
+• No message
+• 10€ Minimum
+
+📌 Don't follow these instructions before sending = no product
+
+If not succeeded, please make a **#📬〢tickets**."""
         )
 
     await bot.process_commands(message)
